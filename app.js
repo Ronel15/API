7d580063f8a0f7ace56b90913085f4b8b44f560f
Carlos-Pech <carlospech247@gmail.com>
2023-02-23 22:03:03 -0600

    Organizacion5

--- app.js
+++ app.js
@@ -21,6 +21,18 @@ const swaggerDocument = require('./swagger');
 const app = express();
 
 app.use(cors());
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+      return res.status(200).json({});
+    }
+  
+    next();
+  });
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
