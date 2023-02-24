0c0cdbcf2d3e384decca9fbd81c955fd89c59ce1
Carlos-Pech <carlospech247@gmail.com>
2023-02-23 23:10:13 -0600

    Organizacion12

--- config/config.js
+++ config/config.js
@@ -2,14 +2,7 @@ module.exports={
     SECRET_TOKEN: process.env.SECRET_TOKEN || 'secreta'
 }
 
-// const IMG_BASE = process.env.IMG || "https://apifoodmet.up.railway.app";
-// const IMG_UPLOADS = "/uploads/";
-// const IMG_URL = IMG_BASE + IMG_UPLOADS + req.file.filename;
+const IMG_BASE = process.env.IMG || "https://apifoodmet.up.railway.app";
+const IMG_UPLOADS = "/uploads/";
+const IMG_URL = IMG_BASE + IMG_UPLOADS + req.file.filename;
 
-// // ...
-
-// if (req.file) {
-//   product.image = IMG_URL;
-// }
-
-// // ...
