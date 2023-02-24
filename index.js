a8b3857aa5c22d4cd53de2f1d09c80d7f1955ac7
Carlos-Pech <carlospech247@gmail.com>
2023-02-24 00:16:22 -0600

    Organizacion17

--- index.js
+++ index.js
@@ -3,8 +3,22 @@ const app = require('./app');
 
 const {swaggerDocs: V1SwaggerDocs} = require('./swagger')
 const PORT = process.env.PORT || 3050;
+app.use(cors());
+// CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+      return res.status(200).json({});
+    }
+  
+    next();
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is running on port http://localhost:${PORT}`);
     V1SwaggerDocs(app, PORT);
+    
 });
