08f96c6a7dee8949622da3e2a04d8343ead3597e
Carlos-Pech <carlospech247@gmail.com>
2023-03-10 23:58:36 -0600

    carrito2

--- models/Cart2.js
+++ models/Cart2.js
@@ -24,12 +24,48 @@ const Schema = mongoose.Schema;
 // module.exports = Cart;
 
 
-const cartSchema = new mongoose.Schema({
-  Products: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Products'
+// const cartSchema = new mongoose.Schema({
+//   Products: [{
+//     type: mongoose.Schema.Types.ObjectId,
+//     ref: 'Products'
+//   }]
+// });
+const cartSchema = new Schema({
+  productos: [{
+    _id: {
+      type: Schema.Types.ObjectId,
+      ref: 'Product'
+    },
+    name: String,
+    description: String,
+    price: Number,
+    image: String,
+    totalCalories: Number,
+    nutrition: Boolean,
+    category: {
+      type: Schema.Types.ObjectId,
+      ref: 'Category'
+    },
+    subcategory: {
+      type: Schema.Types.ObjectId,
+      ref: 'Subcategory'
+    },
+    ingredients: [{
+      value: String,
+      name: String,
+      calories: Number,
+      unidad: String,
+      weight: String
+    }],
+    cantidad: {
+      type: Number,
+      default: 1
+    }
   }]
+}, {
+  timestamps: true
 });
+
 
 const Cart = mongoose.model('Cart', cartSchema);
 
